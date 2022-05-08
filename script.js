a6380e43327eb26ed2f422755d5129c5efc0f901
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-05-08 16:21:36 -0300

    Refatoração do script

--- script.js
+++ script.js
@@ -1,98 +1,52 @@
 function byId(id) {
-  const element = document.getElementById(id);
-  return element;
+  return document.getElementById(id);
 }
 
-const emailInput = byId('email');
-const passwordInput = byId('password');
-
-const button = byId('btn-form-header');
-button.onclick = (event) => {
+byId('btn-form-header').onclick = (event) => {
   event.preventDefault();
-  const emailValue = emailInput.value;
-  const passwordValue = passwordInput.value;
 
-  if (emailValue === 'tryber@teste.com' && passwordValue === '123456') {
+  if (byId('email').value === 'tryber@teste.com' && byId('password').value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
 };
 
-/* O botão só pode ser habilitado se a caixa de seleção estiver selecionada */
-const agreement = byId('agreement');
-agreement.onchange = (event) => {
+byId('agreement').onchange = (event) => {
   byId('submit-btn').disabled = !event.target.checked;
 };
 
-const textArea = byId('textarea');
-textArea.addEventListener('keyup', () => {
-  const caracteresRestantes = 500 - textArea.value.length;
-  const count = document.getElementById('counter');
-  count.innerHTML = `Caracteres : ${caracteresRestantes}`;
+byId('textarea').addEventListener('keyup', (event) => {
+  const caracteresRestantes = 500 - event.target.value.length;
+  byId('counter').innerHTML = `Caracteres : ${caracteresRestantes}`;
 });
 
-function addToForm(array) {
-  let result = '<div class="result">\n';
-  console.log(array);
-  for (let index = 0; index < array.length; index += 1) {
-    console.log(index);
-    result += `<p>${array[index]}</p>\n`;
-  }
+function concatItems(textValues, checkedItems) {
+  const radioFamily = document.querySelector('input[name="family"]:checked');
+  const radioRate = document.querySelector('input[name="rate"]:checked');
+  const result = `<div class="result">
+  <p>Nome: ${textValues[0]} ${textValues[1]}</p>
+  <p>Email: ${textValues[2]}</p>
+  <p>Casa: ${textValues[3]}</p>
+  <p>Família: ${(radioFamily) ? radioFamily.value : ''}</p>
+  <p>Matérias: ${checkedItems.join(', ')}</p>
+  <p>Avaliação: ${(radioRate) ? radioRate.value : ''}</p>
+  <p>Observações: ${textValues[4]}</p>
+</div>`;
 
-  result += '</div>';
-  console.log(result);
   byId('evaluation-form').innerHTML = result;
 }
 
-function concatItems(allValues, checkedItems) {
-  const nome = `Nome: ${allValues[0]} ${allValues[1]}`;
-  const email = `Email: ${allValues[2]}`;
-  const casa = `Casa: ${allValues[3]}`;
-  const familia = `Família: ${allValues[5]}`;
-  let materias = 'Matérias: ';
-  const avaliacao = `Avaliação: ${allValues[6]}`;
-  const observacao = `Observações: ${allValues[4]}`;
-
-  for (let index = 0; index < checkedItems.length; index += 1) {
-    materias += checkedItems[index];
-    if (index !== checkedItems.length - 1) materias += ', ';
-  }
-
-  const array = [nome, email, casa, familia, materias, avaliacao, observacao];
-  console.log(array);
-  addToForm(array);
-}
-
 const ids = ['input-name', 'input-lastname', 'input-email', 'house', 'textarea'];
-const names = ['radio', 'checkbox'];
 
-function type(elementType, element, array) {
-  if (elementType === 'radio' && element.checked) {
-    array[0].push(element.value);
-  }
-  if (elementType === 'checkbox' && element.checked) {
-    array[1].push(element.value);
-  }
-}
-
-const submitButton = byId('submit-btn');
-submitButton.onclick = (event) => {
+byId('submit-btn').onclick = (event) => {
   event.preventDefault();
-
-  const allValues = [];
+  const textInputs = [];
   for (let index = 0; index < ids.length; index += 1) {
-    allValues.push(byId(ids[index]).value);
+    textInputs.push(byId(ids[index]).value);
   }
-
   const checkedItems = [];
-  for (let index = 0; index < names.length; index += 1) {
-    const radios = document.querySelectorAll(`input[type='${names[index]}']`);
-    for (let secondIndex = 0; secondIndex < radios.length; secondIndex += 1) {
-      type(names[index], radios[secondIndex], [allValues, checkedItems]);
-    }
-  }
-
-  checkedItems.pop();
-  concatItems(allValues, checkedItems);
+  const checks = document.querySelectorAll('input[name="content"]:checked');
+  checks.forEach((element) => checkedItems.push(element.value));
+  concatItems(textInputs, checkedItems);
 };
